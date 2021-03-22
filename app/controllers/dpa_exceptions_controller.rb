8d46a3bc58a19d94072d3ba0b191d9885ff44380
Margarita Barvinok <brita@umich.edu>
2021-03-22 14:56:51 -0400

    Add active_storage and attachment to dpa_exceptions; had_many_attached is not working

--- app/controllers/dpa_exceptions_controller.rb
+++ app/controllers/dpa_exceptions_controller.rb
@@ -3,7 +3,7 @@ class DpaExceptionsController < InheritedResources::Base
   private
 
     def dpa_exception_params
-      params.require(:dpa_exception).permit(:review_date, :third_party_product_service, :used_by, :point_of_contact, :review_findings, :review_summary, :lsa_security_recommendation, :lsa_security_determination, :lsa_security_approval, :lsa_technology_services_approval, :exception_approval_date, :notes, :tdx_ticket, :sla_agreement, :data_type_id)
+      params.require(:dpa_exception).permit(:review_date, :third_party_product_service, :used_by, :point_of_contact, :review_findings, :review_summary, :lsa_security_recommendation, :lsa_security_determination, :lsa_security_approval, :lsa_technology_services_approval, :exception_approval_date, :notes, :sla_agreement, :sla_attachment, :data_type_id, :attachment)
     end
 
 end
