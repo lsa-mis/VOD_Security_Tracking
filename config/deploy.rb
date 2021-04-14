f1e91c80e90e4e07f991305e287a94d211621fa2
Rick Smoke <rsmoke@umich.edu>
2021-04-14 17:22:05 -0400

    do not use afer without a second argument

--- config/deploy.rb
+++ config/deploy.rb
@@ -103,7 +103,6 @@ namespace :deploy do
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
   after  :finishing,    :restart
-  after "deploy:updated"
 end
 
 namespace :maintenance do
