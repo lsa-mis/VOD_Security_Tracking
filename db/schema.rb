b725632cc8be275285cc494012e7eb291df92716
Rick Smoke <rsmoke@umich.edu>
2021-04-20 10:31:48 -0400

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_19_135849) do
+ActiveRecord::Schema.define(version: 2021_03_31_233130) do
 
   create_table "active_admin_comments", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "namespace"
