c529521006c5197338ebcb5383b59a0086fd28b8
Rick Smoke <rsmoke@umich.edu>
2021-04-20 15:39:04 -0400

    taking educated random stabs in the dark

--- Capfile
+++ Capfile
@@ -37,6 +37,7 @@ install_plugin Capistrano::Puma
 install_plugin Capistrano::Puma::Systemd
 install_plugin Capistrano::Puma::Workers
 install_plugin Capistrano::Puma::Nginx
+install_plugin Capistrano::Puma::Systemd
 
 # Load custom tasks from `lib/capistrano/tasks` if you have any defined
 Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -24,9 +24,11 @@ set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_user, fetch(:user)
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+set :puma_systemctl_user, :user
 # Avoid permissions issues with using /tmp
 set :tmp_dir, '/home/deployer/tmp'
 
