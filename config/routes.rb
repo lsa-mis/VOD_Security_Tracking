120c70b26a5d3709aee2d9cb0dfbe1b205924845
Margarita Barvinok <brita@umich.edu>
2021-04-23 11:27:46 -0400

    Add routes for archive method

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  post 'archive_it_security_incident/:id', to: 'it_security_incidents#archive', as: :archive_it_security_incident
   resources :it_security_incidents
+
+  post 'archive_sensitive_data_system/:id', to: 'sensitive_data_systems#archive', as: :archive_sensitive_data_system
   resources :sensitive_data_systems
+
+  post 'archive_legacy_os_record/:id', to: 'legacy_os_records#archive', as: :archive_legacy_os_record
   resources :legacy_os_records
+
   get 'dpa_exceptions/audit_log', to: 'dpa_exceptions#audit_log'
+  post 'archive_dpa_exception/:id', to: 'dpa_exceptions#archive', as: :archive_dpa_exception
   resources :dpa_exceptions
+
   resources :devices
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
