daeebdb84004f687d398aae73213a3810b06f043
rsmokeUM <rsmoke@umich.edu>
2021-04-27 11:19:56 -0400

    Update dpa_exceptions_controller.rb

--- app/controllers/dpa_exceptions_controller.rb
+++ app/controllers/dpa_exceptions_controller.rb
@@ -1,6 +1,5 @@
 class DpaExceptionsController < InheritedResources::Base
-  before_action :set_tickets_destroy_path
-
+  
   def destroy
     @dpa_exception = DpaException.find(params[:id])
     authorize @dpa_exception
@@ -19,10 +18,6 @@ class DpaExceptionsController < InheritedResources::Base
   end
 
   private
-
-    def set_tickets_destroy_path
-      @tickets_destroy_path = 'dpa_exception_tdx_ticket_path(params[:id],tdx.id)'
-    end
 
     def dpa_exception_params
       params.require(:dpa_exception).permit(:review_date, :third_party_product_service, :used_by, :point_of_contact, :review_findings, :review_summary, :lsa_security_recommendation, :lsa_security_determination, :lsa_security_approval, :lsa_technology_services_approval, :exception_approval_date, :notes, :sla_agreement, :sla_attachment, :data_type_id, attachments: [])
