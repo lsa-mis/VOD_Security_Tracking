516109b183b5ce08236c23622351161bfba0a72c
Margarita Barvinok <brita@umich.edu>
2021-05-02 16:42:02 -0400

    Improve create and api methods to search TDX database by serial and hostname and check results for only one return

--- app/controllers/legacy_os_records_controller.rb
+++ app/controllers/legacy_os_records_controller.rb
@@ -13,12 +13,12 @@ class LegacyOsRecordsController < InheritedResources::Base
   def create
     @legacy_os_record = LegacyOsRecord.new(legacy_os_record_params)
     serial = legacy_os_record_params[:device_attributes][:serial]
-    # check if serial exists in devices table 
+    # check if serial (or hostname) exists in devices table 
     if Device.find_by(serial: serial).present?
       @legacy_os_record.device_id = Device.find_by(serial: serial).id
+    elsif Device.find_by(hostname: serial).present?
+      @legacy_os_record.device_id = Device.find_by(hostname: serial).id
     else
-      dev = @legacy_os_record.build_device(serial: legacy_os_record_params[:device_attributes][:serial])
-      # get device data from API
       #  get auth token
       url = URI("https://apigw.it.umich.edu/um/it/oauth2/token")
 
@@ -33,7 +33,9 @@ class LegacyOsRecordsController < InheritedResources::Base
 
       response = http.request(request)
       access_token = JSON.parse(response.read_body)['access_token']
-      serial = legacy_os_record_params[:device_attributes][:serial]    
+
+      # get device data from API
+      serial = legacy_os_record_params[:device_attributes][:serial]
 
       url = URI("https://apigw.it.umich.edu/um/it/48/assets/search")
 
@@ -51,44 +53,62 @@ class LegacyOsRecordsController < InheritedResources::Base
       response = http.request(request)
       asset_info = JSON.parse(response.read_body)
 
-      # check if response is not empty
-      if asset_info.present? && asset_info[0]['SerialNumber'] == serial
-        asset_id = asset_info[0]['ID']
-
-        url = URI("https://apigw.it.umich.edu/um/it/48/assets/#{asset_id}")
-
-        request = Net::HTTP::Get.new(url)
-        request["x-ibm-client-id"] = "#{Rails.application.credentials.um_api[:tdx_client_id]}"
-        request["authorization"] = "Bearer #{access_token}"
-        request["accept"] = 'application/json'
-
-        response = http.request(request)
-        asset_info = JSON.parse(response.read_body)
-
-        dev.building = asset_info['LocationName']
-        dev.room = asset_info['LocationRoomName']
-        dev.hostname = asset_info['Name']
-        dev.owner = asset_info['OwningCustomerName']
-        dev.department = asset_info['OwningDepartmentName']
-        dev.manufacturer = asset_info['ManufacturerName']
-        dev.model = asset_info['ProductModelName']
-
-        asset_info['Attributes'].each do |att|
-          if att['Name'] == "MAC Address(es)"
-            dev.mac = att['Value']
+      # check if response is not empty and returns only one result
+      if asset_info.present? && asset_info.count == 1
+          # serial or hostname = :serial
+          if asset_info[0]['SerialNumber'] == serial
+            dev = @legacy_os_record.build_device(serial: legacy_os_record_params[:device_attributes][:serial])
+            dev.hostname = asset_info[0]['Name']
           end
-        end
-      else
+          if asset_info[0]['Name'] == serial
+            dev = @legacy_os_record.build_device(hostname: legacy_os_record_params[:device_attributes][:serial])
+            dev.serial = asset_info[0]['SerialNumber']
+          end
+          asset_id = asset_info[0]['ID']
+          # get attributes by asset_id
+          url = URI("https://apigw.it.umich.edu/um/it/48/assets/#{asset_id}")
+
+          request = Net::HTTP::Get.new(url)
+          request["x-ibm-client-id"] = "#{Rails.application.credentials.um_api[:tdx_client_id]}"
+          request["authorization"] = "Bearer #{access_token}"
+          request["accept"] = 'application/json'
+
+          response = http.request(request)
+          asset_info = JSON.parse(response.read_body)
+
+          dev.building = asset_info['LocationName']
+          dev.room = asset_info['LocationRoomName']
+          dev.owner = asset_info['OwningCustomerName']
+          dev.department = asset_info['OwningDepartmentName']
+          dev.manufacturer = asset_info['ManufacturerName']
+          dev.model = asset_info['ProductModelName']
+
+          asset_info['Attributes'].each do |att|
+            if att['Name'] == "MAC Address(es)"
+              dev.mac = att['Value']
+            end
+          end
+      elsif asset_info.present? && asset_info.count > 1
+        error_device = "More then one result returned for serial or hostname [#{serial}]"
+      else 
         device_note = "This device is not present in the TDX Assets database"
       end
       # end of API
     end
     respond_to do |format|
       device_note = "" if device_note.nil?
-      if @legacy_os_record.save        
-        format.html { redirect_to legacy_os_record_path(@legacy_os_record), notice: 'legacy os record was successfully created. ' + device_note}
-        format.json { render :show, status: :created, location: @legacy_os_record }
+      error_device = "" if error_device.nil?
+      if error_device.blank?
+        if @legacy_os_record.save        
+          format.html { redirect_to legacy_os_record_path(@legacy_os_record), notice: 'legacy os record was successfully created. ' + device_note}
+          format.json { render :show, status: :created, location: @legacy_os_record }
+        else
+          format.html { render :new }
+          format.json { render json: @legacy_os_record.errors, status: :unprocessable_entity }
+        end
       else
+        logger.debug "***************************************error_device: #{error_device}"
+        flash.now[:alert] = error_device
         format.html { render :new }
         format.json { render json: @legacy_os_record.errors, status: :unprocessable_entity }
       end
