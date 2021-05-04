2ac0cd67a47443d0af8c9b3c4df91023bf252c8a
Margarita Barvinok <brita@umich.edu>
2021-05-04 14:20:33 -0400

    Add edit method with API call

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -25,7 +25,6 @@ class DevicesController < InheritedResources::Base
       # call DeviceTdxApi module
       get_device_data
     end
-
     respond_to do |format|
       @device_note = "" if @device_note.nil?
       @error_device = "" if @error_device.nil?
@@ -44,6 +43,37 @@ class DevicesController < InheritedResources::Base
       end
     end
   end
+
+  def update
+    @device = Device.find(params[:id])
+    @serial = device_params[:serial]
+    @hostname = device_params[:hostname]
+    if @serial.present? 
+      @search_field = @serial
+    else 
+      @search_field = @hostname
+    end
+    # call DeviceTdxApi module
+    get_device_data
+    respond_to do |format|
+      @device_note = "" if @device_note.nil?
+      @error_device = "" if @error_device.nil?
+      if @error_device.blank? && @device_note.blank?
+        if @device.save
+          format.html { redirect_to @device, notice: "device was successfully updated." }
+          format.json { render :show, status: :created, location: @device }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @device.errors, status: :unprocessable_entity }
+        end
+      else
+        flash.now[:alert] = @error_device + @device_note 
+        format.html { render :edit }
+        format.json { render json: @device.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
     def device_params
