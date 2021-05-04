ec6c3ecd0cbbb33e91d001e7ed51f363697a2206
Margarita Barvinok <brita@umich.edu>
2021-05-04 14:02:03 -0400

    Use DeviceTdxApi module instead of build in method

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -2,88 +2,44 @@ class DevicesController < InheritedResources::Base
   devise_group :logged_in, contains: [:user, :admin_user]
   before_action :authenticate_logged_in!
 
-  require 'uri'
-  require 'net/http'
-  require 'openssl'
-  require 'json'
+  include DeviceTdxApi
 
   def new
     @device = Device.new
   end
 
   def create
-
-    #  get auth token
-    url = URI("https://apigw.it.umich.edu/um/it/oauth2/token")
-
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-    request = Net::HTTP::Post.new(url)
-    request["content-type"] = 'application/x-www-form-urlencoded'
-    request["accept"] = 'application/json'
-    request.body = "grant_type=client_credentials&client_id=#{Rails.application.credentials.um_api[:tdx_client_id]}&client_secret=#{Rails.application.credentials.um_api[:tdx_client_secret]}&scope=tdxticket"
-
-    response = http.request(request)
-    access_token = JSON.parse(response.read_body)['access_token']
-
-    @device = Device.new(device_params)
-    serial = params[:device][:serial]
-
-    url = URI("https://apigw.it.umich.edu/um/it/48/assets/search")
-
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-    request = Net::HTTP::Post.new(url)
-    request["x-ibm-client-id"] = "#{Rails.application.credentials.um_api[:tdx_client_id]}"
-    request["authorization"] = "Bearer #{access_token}"
-    request["content-type"] = 'application/json'
-    request["accept"] = 'application/json'
-    request.body = "{\"SerialLike\":\"#{serial}\"}"
-
-    response = http.request(request)
-    asset_info = JSON.parse(response.read_body)
-
-    # check if response is not empty
-    if asset_info.present?
-      asset_id = asset_info[0]['ID']
-
-      url = URI("https://apigw.it.umich.edu/um/it/48/assets/#{asset_id}")
-
-      request = Net::HTTP::Get.new(url)
-      request["x-ibm-client-id"] = "#{Rails.application.credentials.um_api[:tdx_client_id]}"
-      request["authorization"] = "Bearer #{access_token}"
-      request["accept"] = 'application/json'
-
-      response = http.request(request)
-      asset_info = JSON.parse(response.read_body)
-
-      @device.building = asset_info['LocationName']
-      @device.room = asset_info['LocationRoomName']
-      @device.hostname = asset_info['Name']
-      @device.owner = asset_info['OwningCustomerName']
-      @device.department = asset_info['OwningDepartmentName']
-      @device.manufacturer = asset_info['ManufacturerName']
-      @device.model = asset_info['ProductModelName']
-
-      asset_info['Attributes'].each do |att|
-        if att['Name'] == "MAC Address(es)"
-          @device.mac = att['Value']
-        end
-      end
+    @device = Device.new(device_params)    
+    @serial = device_params[:serial]
+    @hostname = device_params[:hostname]
+    if @serial.present? 
+      @search_field = @serial
+    else 
+      @search_field = @hostname
+    end
+    if @serial.present? && Device.find_by(serial: @serial).present?
+        @device_note = "This device with serial number [#{@serial}] already exist"
+    elsif @hostname.present? && Device.find_by(hostname: @hostname).present?
+        @device_note = "This device with hostname [#{@hostname}] already exist"
     else
-      redirect_to devices_path, alert: "#{serial} is not in the assets database. Please add this computer to the inventory first." and return
+      # call DeviceTdxApi module
+      get_device_data
     end
 
     respond_to do |format|
-      if @device.save
-        format.html { redirect_to @device, notice: "device was successfully created." }
-        format.json { render :show, status: :created, location: @device }
+      @device_note = "" if @device_note.nil?
+      @error_device = "" if @error_device.nil?
+      if @error_device.blank? && @device_note.blank?
+        if @device.save
+          format.html { redirect_to @device, notice: "device was successfully created." }
+          format.json { render :show, status: :created, location: @device }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @device.errors, status: :unprocessable_entity }
+        end
       else
-        format.html { render :new, status: :unprocessable_entity }
+        flash.now[:alert] = @error_device + @device_note 
+        format.html { render :new }
         format.json { render json: @device.errors, status: :unprocessable_entity }
       end
     end
