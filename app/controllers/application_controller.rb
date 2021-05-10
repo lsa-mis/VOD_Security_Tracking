db95cd1d1c63f9a62fb5b526933e397aca37651e
Ananta old mac <anantas_old@lit-x-anantas.local>
2021-05-10 11:58:49 -0400

    LRA-3 Application controller changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+  rescue_from DeviseLdapAuthenticatable::LdapException do |exception|
+    render :text => exception, :status => 500
+  end
+  def after_sign_in_path_for(resource)
+    home_path
+  end  
     include Pundit
     rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
