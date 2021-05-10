6223a8b409bc260a7ceea8f0248e4026c2f87874
Ananta old mac <anantas_old@lit-x-anantas.local>
2021-05-10 12:06:36 -0400

    LRA-3 route changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+  devise_scope :user do
+    root to: 'users/sessions#new'
+    get 'sign_in', to: 'users/sessions#new'
+    get '/users/sign_out', to: 'users/sessions#destroy'
+  end
   resources :it_security_incidents
   resources :sensitive_data_systems
   resources :legacy_os_records
@@ -9,7 +16,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'static_pages#home'
+  get 'home', to: 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/privacy', to: 'static_pages#privacy'
   get '/terms', to: 'static_pages#terms'
