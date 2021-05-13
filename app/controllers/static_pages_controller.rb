9f3ff39aed0510364145125c12305b0b17c05a2e
Rick Smoke <rsmoke@umich.edu>
2021-05-13 18:28:47 -0400

    leaned up margins and started to flush out dynamic breadcrumbs

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,10 @@
 class StaticPagesController < ApplicationController
+  add_breadcrumb "home", :root_path
+
   def home
   end
 
   def dashboard
+    add_breadcrumb "dashboard", dashboard_path
   end
 end
