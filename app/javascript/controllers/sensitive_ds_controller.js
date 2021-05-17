8e5b31a4a692462c68bc8e5b98efcaa182e49f4b
Margarita Barvinok <brita@umich.edu>
2021-05-17 11:05:10 -0400

    Rename controller.js

---
+++ app/javascript/controllers/sensitive_ds_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = ["sensitive_data_system_type", "system_device"]
+
+  connect() {
+    console.log("hello from StimulusJS - hell!!!!!")
+    this.display_device()
+  }
+
+  display_device() {
+    console.log("display device")
+    var type = this.sensitive_data_system_typeTarget.value
+    console.log("type: ")
+    console.log(type)
+    if (type == 1) {
+      this.system_deviceTarget.classList.add("device--display")
+      this.system_deviceTarget.classList.remove("device--hide")
+    }
+    else {
+      this.system_deviceTarget.classList.add("device--hide")
+      this.system_deviceTarget.classList.remove("device--display")
+    }
+  }
+}
