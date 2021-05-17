64163256321d2db8e5ef4127bf5dc786c9c26864
Margarita Barvinok <brita@umich.edu>
2021-05-17 11:20:08 -0400

    Restore hello controller

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -7,28 +7,16 @@
 //   <h1 data-target="hello.output"></h1>
 // </div>
 
-import { Controller } from "stimulus"
-
+import { Controller } from 'stimulus';
 export default class extends Controller {
-  static targets = ["sensitive_data_system_type", "system_device"]
+  static targets = ["heading"]
 
   connect() {
-    console.log("hello from StimulusJS - hell!!!!!")
-    this.display_device()
+    console.log("hello from StimulusJS")
   }
 
-  display_device() {
-    console.log("display device")
-    var type = this.sensitive_data_system_typeTarget.value
-    console.log("type: ")
-    console.log(type)
-    if (type == 1) {
-      this.system_deviceTarget.classList.add("device--display")
-      this.system_deviceTarget.classList.remove("device--hide")
-    }
-    else {
-      this.system_deviceTarget.classList.add("device--hide")
-      this.system_deviceTarget.classList.remove("device--display")
-    }
+  greet() {
+    console.log("click")
+    this.headingTarget.innerHTML = "Hello World"
   }
 }
