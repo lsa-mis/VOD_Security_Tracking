bff5816ca0338b663c35021cc9b370949301489b
Margarita Barvinok <brita@umich.edu>
2021-05-17 10:31:57 -0400

    Display/hide device fileds

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -13,29 +13,22 @@ export default class extends Controller {
   static targets = ["sensitive_data_system_type", "system_device"]
 
   connect() {
-    // this.outputTarget.textContent = 'Hello, Stimulus!'
     console.log("hello from StimulusJS - hell!!!!!")
-    // var type_val = this.targets.find("sensitive_data_system_type").value();
-    // console.log("value: ")
-    // console.log(type_val)
-
-    // var type = this.sensitive_data_system_typeTarget
-    // console.log("type: ")
-    // console.log(type)
-    // var val = type.value
-    // console.log("val: ")
-
-    // console.log(val)
-
+    this.display_device()
   }
 
   display_device() {
-    alert("display device")
+    console.log("display device")
     var type = this.sensitive_data_system_typeTarget.value
     console.log("type: ")
     console.log(type)
     if (type == 1) {
-      this.system_deviceTarget.classList.toggle("device--hide")
+      this.system_deviceTarget.classList.add("device--display")
+      this.system_deviceTarget.classList.remove("device--hide")
+    }
+    else {
+      this.system_deviceTarget.classList.add("device--hide")
+      this.system_deviceTarget.classList.remove("device--display")
     }
   }
 }
