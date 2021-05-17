88e751b33dc7307c96f7121846c306bbc93c17fe
Margarita Barvinok <brita@umich.edu>
2021-05-17 08:40:54 -0400

    Fix error because of merging branch

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     sessions: 'users/sessions'
   }
   devise_scope :user do
-    root to: 'users/sessions#new'
+    # root to: 'users/sessions#new'
     get 'sign_in', to: 'users/sessions#new'
     get '/users/sign_out', to: 'users/sessions#destroy'
   end
