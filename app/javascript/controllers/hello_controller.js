091b4b4316e3e92e4e1ef6b35111131521b4a386
Margarita Barvinok <brita@umich.edu>
2021-05-17 13:21:35 -0400

    again hello controller works; sensitive_ds - doesn't

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -7,16 +7,42 @@
 //   <h1 data-target="hello.output"></h1>
 // </div>
 
-import { Controller } from 'stimulus';
+// import { Controller } from 'stimulus';
+// export default class extends Controller {
+//   static targets = ["heading"]
+
+//   connect() {
+//     console.log("hello from StimulusJS")
+//   }
+
+//   greet() {
+//     console.log("click")
+//     this.headingTarget.innerHTML = "Hello World"
+//   }
+// }
+
+import { Controller } from "stimulus"
+
 export default class extends Controller {
-  static targets = ["heading"]
+  static targets = ["sensitive_data_system_type", "system_device"]
 
   connect() {
-    console.log("hello from StimulusJS")
+    console.log("hello from StimulusJS - hell!!!!!")
+    this.display_device()
   }
 
-  greet() {
-    console.log("click")
-    this.headingTarget.innerHTML = "Hello World"
+  display_device() {
+    console.log("display device")
+    var type = this.sensitive_data_system_typeTarget.value
+    console.log("type: ")
+    console.log(type)
+    if (type == 1) {
+      this.system_deviceTarget.classList.add("device--display")
+      this.system_deviceTarget.classList.remove("device--hide")
+    }
+    else {
+      this.system_deviceTarget.classList.add("device--hide")
+      this.system_deviceTarget.classList.remove("device--display")
+    }
   }
 }
