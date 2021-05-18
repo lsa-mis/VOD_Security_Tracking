b54258fbeb65ae492e4a4cae128cafd6f29db9f8
Rick Smoke <rsmoke@umich.edu>
2021-05-18 09:07:46 -0400

    cleanup apllication controller format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,32 +1,32 @@
 class ApplicationController < ActionController::Base
-before_action :set_breadcrumbs
-  rescue_from DeviseLdapAuthenticatable::LdapException do |exception|
-    render :text => exception, :status => 500
-  end
+  before_action :set_breadcrumbs
 
   include Pundit
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
- def add_breadcrumb(label, path = nil)
-  @breadcrumbs << {
-    label: label,
-    path: path
-  }
- end
+  rescue_from DeviseLdapAuthenticatable::LdapException do |exception|
+    render :text => exception, :status => 500
+  end
 
- def set_breadcrumbs
-  @breadcrumbs = []
- end
-
-
-  def after_sign_in_path_for(resource)
-    dashboard_path
-  end  
+  def add_breadcrumb(label, path = nil)
+    @breadcrumbs << {
+      label: label,
+      path: path
+    }
+  end
 
   private
 
-  def user_not_authorized
-    flash[:alert] = "You are not authorized to perform this action."
-    redirect_to(request.referrer || root_path)
-  end
+    def set_breadcrumbs
+      @breadcrumbs = []
+    end
+
+    def after_sign_in_path_for(resource)
+      dashboard_path
+    end  
+
+    def user_not_authorized
+      flash[:alert] = "You are not authorized to perform this action."
+      redirect_to(request.referrer || root_path)
+    end
 end
