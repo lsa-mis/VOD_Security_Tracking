0024ac1b3d75203bf559d194eb1ede462a24def9
Rick Smoke <rsmoke@umich.edu>
2021-05-18 20:07:07 -0400

    incorporate styles

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "../layouts/application.sass";
+import "../layouts/application.sass"
+import "../layouts/forms.sass"
 
 Rails.start()
 Turbolinks.start()
