589bdc0a7c7b4cbd37d4f8f7621e72af19a85168
Rick Smoke <rsmoke@umich.edu>
2021-05-25 13:21:07 -0400

    add file delete route

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
   end
 
   get 'dpa_exceptions/audit_log', to: 'dpa_exceptions#audit_log'
+  get 'dpa_exceptions/delete_file_attachment/:id', to: 'dpa_exceptions#delete_file_attachment', as: :delete_sla_agreement
   resources :dpa_exceptions do
     resources :tdx_tickets, module: :dpa_exceptions
   end
