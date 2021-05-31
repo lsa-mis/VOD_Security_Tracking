398289ecf97bb741db00af5f6215f34f5c9527bd
Margarita Barvinok <brita@umich.edu>
2021-05-31 16:57:03 -0400

    Add access_lookup table

---
+++ app/admin/access_lookups.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register AccessLookup do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  permit_params :ldap_group, :table, :action
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:ldap_group, :table, :action]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
---
+++ spec/factories/access_lookups.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: access_lookups
+#
+#  id         :bigint           not null, primary key
+#  ldap_group :string(255)
+#  table      :string(255)
+#  action     :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+FactoryBot.define do
+  factory :access_lookup do
+    ldap_group { "MyString" }
+    table { "MyString" }
+    action { "MyString" }
+  end
+end
