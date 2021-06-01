ce14e43af4e87315d1acfe6d8fcc6ed0e14510e0
Rick Smoke <rsmoke@umich.edu>
2021-06-01 11:50:10 -0400

    after_login to set group membership in session

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Users::SessionsController < Devise::SessionsController
    before_action :configure_sign_in_params, only: [:create]
+   after_action :after_login
 
   # GET /resource/sign_in
    def new
@@ -24,4 +25,20 @@ class Users::SessionsController < Devise::SessionsController
    def configure_sign_in_params
      devise_parameter_sanitizer.permit(:sign_in, keys: [:username, :email, :membership])
    end
+
+
+   private
+
+    def after_login
+      if user_signed_in?
+        membership = []
+        groups = Devise::LDAP::Adapter.get_ldap_param(current_user.username,'memberOf')
+        groups.each do |group|
+          g = group.split(',')
+          membership.append(g.first.remove("CN="))
+        end
+        session[:user_memberships] = membership
+        # logger.debug "*********** session[:user_memberships] ***** #{session[:user_memberships]}"
+      end
+    end
 end
