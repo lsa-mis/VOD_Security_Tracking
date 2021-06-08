57439ae29511ee44e2d093864e7daaecf02a0ee1
Margarita Barvinok <brita@umich.edu>
2021-06-08 17:23:06 -0400

    Fix lookup query

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,10 @@ module ApplicationHelper
 
   def has_ad_access?(user, table, action=nil)
     user.membership = session[:user_memberships]
-    # logger.debug "*********************************user.membership: #{user.membership}"
-    # logger.debug "*********************************has_ad_access: #{action}"
     if action
       ldap_groups = AccessLookup.where(table: table, :action => [action, 'all_actions']).pluck(:ldap_group)
     else
-      ldap_groups = AccessLookup.where(table: table).pluck(:ldap_group)
+      ldap_groups = AccessLookup.where(table: table, :action => ['newedit_action', 'show_action', 'archive_action', 'all_actions']).pluck(:ldap_group)
     end
     (user.membership & ldap_groups).any?
   end
