cb40a5cccf6263c0dfd299ed4ce90d88f6331749
Margarita Barvinok <brita@umich.edu>
2021-06-11 13:59:58 -0400

    Add device_is_required field to storage_locations

--- app/models/storage_location.rb
+++ app/models/storage_location.rb
@@ -2,12 +2,13 @@
 #
 # Table name: storage_locations
 #
-#  id               :bigint           not null, primary key
-#  name             :string(255)
-#  description      :string(255)
-#  description_link :string(255)
-#  created_at       :datetime         not null
-#  updated_at       :datetime         not null
+#  id                 :bigint           not null, primary key
+#  name               :string(255)
+#  description        :string(255)
+#  description_link   :string(255)
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  device_is_required :boolean          default(FALSE)
 #
 class StorageLocation < ApplicationRecord
     has_many :sensitive_data_systems
