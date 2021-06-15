6c896e9fb840c27a1facd3c3dc239cb05d3b2ff6
Margarita Barvinok <brita@umich.edu>
2021-06-15 13:46:24 -0400

    Install tailwindcss-stimulus-components; toggle filters in dpa_ecxeption

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -16,3 +16,14 @@ require("flatpickr/dist/flatpickr.css")
 
 // Manually register Flatpickr as a stimulus controller
 application.register('flatpickr', Flatpickr)
+
+// Import and register all TailwindCSS Components
+import { Alert, Autosave, Dropdown, Modal, Tabs, Popover, Toggle, Slideover } from "tailwindcss-stimulus-components"
+application.register('alert', Alert)
+application.register('autosave', Autosave)
+application.register('dropdown', Dropdown)
+application.register('modal', Modal)
+application.register('tabs', Tabs)
+application.register('popover', Popover)
+application.register('toggle', Toggle)
+application.register('slideover', Slideover)
