05233d6b9e5e382b502a9043793286a31b45ef65
Margarita Barvinok <brita@umich.edu>
2021-06-21 15:25:03 -0400

    fix typo in field name

--- app/models/it_security_incident.rb
+++ app/models/it_security_incident.rb
@@ -7,7 +7,7 @@
 #  people_involved                :text(65535)      not null
 #  equipment_involved             :text(65535)      not null
 #  remediation_steps              :text(65535)      not null
-#  estimated_finacial_cost        :integer
+#  estimated_financial_cost        :integer
 #  notes                          :text(65535)
 #  it_security_incident_status_id :bigint
 #  data_type_id                   :bigint           not null
