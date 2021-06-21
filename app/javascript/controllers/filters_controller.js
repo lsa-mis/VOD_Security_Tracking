595a813eccfe3d26e905340bf545e6aeb5771746
Margarita Barvinok <brita@umich.edu>
2021-06-21 15:25:37 -0400

    Filters

--- app/javascript/controllers/filters_controller.js
+++ app/javascript/controllers/filters_controller.js
@@ -1,8 +1,11 @@
 import { Controller } from "stimulus"
 const Choices = require('choices.js');
 export default class extends Controller {
-  static targets = ["form", "all_filters"]
-  static classes = ['hidden']
+  static targets = ["displayfilters"]
+  static values = { visible: Boolean }
+  static classes = ["hidden"]
+
+
 
 
   //   initialize(){
@@ -17,33 +20,18 @@ export default class extends Controller {
   //     })
   //   }
 
-  connect() {
-    console.log("choices")
-    this.class = this.hasHiddenClass ? this.hiddenClass : 'hidden'
-    console.log(this.hiddenClass)
-    console.log("class")
-    console.log(this.class)
+  // connect() {
+  //   console.log("choices")
+  //   this.updateHiddenClass()
 
-  }
-
-  toggle() {
-    console.log("toggle")
-    this.all_filtersTarget.classList.toggle(this.class)
-  }
-
-  show() {
-    console.log("show")
-    this.all_filtersTarget.classList.remove(this.class)
-  }
-
-  hide() {
-    console.log("hide")
-    this.all_filtersTarget.classList.add(this.class)
-  }
-
-  submitForm(event) {
-    console.log("submit")
-    this.show()
+  // }
+  updateHiddenClass() {
+    console.log("updateHiddenClass")
+    // console.log(this.displayfiltersTarget.classList)
+    // console.log(this.hiddenClass)
+    // console.log(this.visibleValue)
+    this.displayfiltersTarget.classList.toggle(this.hiddenClass, !this.visibleValue)
+    this.visibleValue = !this.visibleValue
   }
 
 }
