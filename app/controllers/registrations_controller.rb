0d22bcaf4030e06d73306bc888b433390728eeb1
Margarita Barvinok <brita@umich.edu>
2021-06-21 09:06:28 -0400

    Atempt (failed) to process duo as HTML

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,9 @@ class RegistrationsController < Devise::RegistrationsController
   skip_before_action :require_no_authentication, only: [:duo_verify]
   skip_before_action :verify_authenticity_token
 
+  self.responder = Responder
+  respond_to :html, :turbo_stream
+  
   def duo
     @sig_request = Duo.sign_request(Rails.application.credentials.duo[:duo_ikey], Rails.application.credentials.duo[:duo_skey], Rails.application.credentials.duo[:duo_akey], current_user.username)
   end
@@ -15,4 +18,5 @@ class RegistrationsController < Devise::RegistrationsController
       redirect_to unauthenticated_root_path
     end
   end
+
 end
