2367ed816f819c07a634424bdf622133d42d3d66
Margarita Barvinok <brita@umich.edu>
2021-07-07 11:10:38 -0400

    fix typo

--- app/controllers/dpa_exceptions_controller.rb
+++ app/controllers/dpa_exceptions_controller.rb
@@ -17,7 +17,7 @@ class DpaExceptionsController < InheritedResources::Base
 
     @dpa_exceptions = @q.result
     @total = @dpa_exceptions.count
-    @dpa_status = @dpa_exceptions.uniq.pluck(:data_type_id)
+    @dpa_status = @dpa_exceptions.uniq.pluck(:dpa_status)
     @used_by = @dpa_exceptions.uniq.pluck(:used_by)
     
     authorize @dpa_exceptions
