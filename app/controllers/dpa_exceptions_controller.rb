b264b326f2d7f663e266d9f38eeb3b1efd6baf4a
Margarita Barvinok <brita@umich.edu>
2021-07-06 22:58:03 -0400

    add dpa_status and edit displayed fields

--- app/controllers/dpa_exceptions_controller.rb
+++ app/controllers/dpa_exceptions_controller.rb
@@ -17,6 +17,7 @@ class DpaExceptionsController < InheritedResources::Base
 
     @dpa_exceptions = @q.result
     @total = @dpa_exceptions.count
+    @dpa_status = @dpa_exceptions.uniq.pluck(:data_type_id)
     @used_by = @dpa_exceptions.uniq.pluck(:used_by)
     
     authorize @dpa_exceptions
