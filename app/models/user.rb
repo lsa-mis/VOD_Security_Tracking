3ddf8373b57277cc24c8c899a7987add6c43d37e
Rick Smoke <rsmoke@umich.edu>
2021-07-08 16:31:03 -0400

    remove recoverable amd associated fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,31 +2,28 @@
 #
 # Table name: users
 #
-#  id                     :bigint           not null, primary key
-#  email                  :string(255)      default("")
-#  encrypted_password     :string(255)      default(""), not null
-#  reset_password_token   :string(255)
-#  reset_password_sent_at :datetime
-#  remember_created_at    :datetime
-#  created_at             :datetime         not null
-#  updated_at             :datetime         not null
-#  sign_in_count          :integer          default(0), not null
-#  current_sign_in_at     :datetime
-#  last_sign_in_at        :datetime
-#  current_sign_in_ip     :string(255)
-#  last_sign_in_ip        :string(255)
-#  failed_attempts        :integer          default(0), not null
-#  unlock_token           :string(255)
-#  locked_at              :datetime
-#  username               :string(255)      default(""), not null
+#  id                  :bigint           not null, primary key
+#  email               :string(255)      default("")
+#  remember_created_at :datetime
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  sign_in_count       :integer          default(0), not null
+#  current_sign_in_at  :datetime
+#  last_sign_in_at     :datetime
+#  current_sign_in_ip  :string(255)
+#  last_sign_in_ip     :string(255)
+#  failed_attempts     :integer          default(0), not null
+#  unlock_token        :string(255)
+#  locked_at           :datetime
+#  username            :string(255)      default(""), not null
 #
 class User < ApplicationRecord
   before_create :set_email_address
   attr_accessor :membership
 
   # Include default devise modules. Others available are:
-  devise :ldap_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :timeoutable, :lockable
+  devise :ldap_authenticatable, :rememberable, :trackable, :timeoutable,
+         :lockable
 
   def display_name
     "#{self.username} - #{email}"
