e8043fed9366be165f77625a414abd7c9a1a79d6
Margarita Barvinok <brita@umich.edu>
2021-07-08 22:05:11 -0400

    run bundle exec rails webpacker:install:erb

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,7 @@
 const { environment } = require('@rails/webpacker')
+const erb = require('./loaders/erb')
 const jquery = require('./plugins/jquery')
 
 environment.plugins.prepend('jquery', jquery)
+environment.loaders.prepend('erb', erb)
 module.exports = environment
