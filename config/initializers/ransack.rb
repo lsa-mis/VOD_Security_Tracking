94f65d38f7a98a95999c04edb2f4efebc638b014
Rick Smoke <rsmoke@umich.edu>
2021-07-12 16:16:34 -0400

    remove ransack.enum

--- config/initializers/ransack.rb
+++ config/initializers/ransack.rb
@@ -3,11 +3,3 @@ Ransack.configure do |c|
     # Default is true (do not raise error on unknown conditions).
     c.ignore_unknown_conditions = false
 end
-
-Ransack::Enum.configure do |config|
-  # enabled
-  config.enabled = true
-
-  # disabled
-  # config.enabled = false
-end
