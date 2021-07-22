a851a244b988ce5dcddeb1298048deb02a4028e3
Margarita Barvinok <brita@umich.edu>
2021-07-22 15:53:32 -0400

    Add settings to pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,11 +2,14 @@ class StaticPagesController < ApplicationController
   before_action :verify_duo_authentication, only: [:dashboard]
 
   def home
+    @home_text = ApplicationSetting.find_by(title: "home")
+
     add_breadcrumb('', root_path)
   end
 
   def dashboard
     @current_username = current_user.nil? ? current_admin_user.username : current_user.username
+    @dashbord_text = ApplicationSetting.find_by(title: "dashbord")
     add_breadcrumb(action_name.titleize)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :application_settings
   root to: 'static_pages#home'
   devise_for :users, controllers: {
     sessions: 'users/sessions'
