bbe46b2c5b153959c701e9c155b1312e27d613e6
Rick Smoke <rsmoke@umich.edu>
2021-07-27 19:33:00 -0400

    migrated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_26_22309) do
+ActiveRecord::Schema.define(version: 2021_07_27_153515) do
 
   create_table "access_lookups", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "ldap_group"
@@ -18,6 +18,16 @@ ActiveRecord::Schema.define(version: 2021_07_26_22309) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "vod_table", default: 0, null: false
     t.integer "vod_action", default: 0, null: false
+  end
+
+  create_table "action_text_rich_texts", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "body", size: :long
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
   end
 
   create_table "active_admin_comments", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
@@ -113,16 +123,6 @@ ActiveRecord::Schema.define(version: 2021_07_26_22309) do
     t.string "department"
     t.string "manufacturer"
     t.string "model"
-    t.datetime "deleted_at"
-  end
-
-  create_table "dpa_exception_searches", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
-    t.datetime "review_date_exception_first_approval_date"
-    t.string "third_party_product_service"
-    t.string "used_by"
-    t.string "point_of_contact"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "dpa_exception_statuses", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
@@ -155,6 +155,12 @@ ActiveRecord::Schema.define(version: 2021_07_26_22309) do
     t.bigint "dpa_exception_status_id", null: false
     t.index ["data_type_id"], name: "index_dpa_exceptions_on_data_type_id"
     t.index ["dpa_exception_status_id"], name: "index_dpa_exceptions_on_dpa_exception_status_id"
+  end
+
+  create_table "infotexts", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
+    t.string "location", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "it_security_incident_statuses", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
@@ -209,15 +215,6 @@ ActiveRecord::Schema.define(version: 2021_07_26_22309) do
     t.boolean "incomplete", default: false
     t.index ["data_type_id"], name: "index_legacy_os_records_on_data_type_id"
     t.index ["device_id"], name: "index_legacy_os_records_on_device_id"
-  end
-
-  create_table "search_dpa_exceptions", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
-    t.datetime "review_date_exception_first_approval_date"
-    t.string "third_party_product_service"
-    t.string "used_by"
-    t.string "point_of_contact"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "sensitive_data_systems", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
