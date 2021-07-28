9880334588d34bb70bcc2cd7dc800f4b8e4dd468
Margarita Barvinok <brita@umich.edu>
2021-07-28 16:37:20 -0400

    Add admin policy for reports

--- app/policies/active_admin/page_policy.rb
+++ app/policies/active_admin/page_policy.rb
@@ -4,6 +4,8 @@ module ActiveAdmin
       case record.name
       when 'Dashboard'
       (user.membership & @admins_groups).any?
+      when 'Reports'
+        (user.membership & @admins_groups).any?
       else
         false
       end
