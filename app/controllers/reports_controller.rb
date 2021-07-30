1b092b3a8c852e99815810161a5a121289e11b58
Margarita Barvinok <brita@umich.edu>
2021-07-30 11:22:23 -0400

    Add another report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -12,9 +12,21 @@ class ReportsController < ApplicationController
     records_array = ActiveRecord::Base.connection.exec_query(sql)
     @result = []
     @result.push({"table" => "legacy_os_records", "header" => records_array.columns, "rows" => records_array.rows, "total" => records_array.count})
-    # @total = records_array.count
-    # @header = @records_array.columns
 
+    render turbo_stream: turbo_stream.replace(
+      :reportListing,
+      partial: "reports/listing")
+
+  end
+
+  def sensitive_data_system_review_date_this_month
+    sql = "SELECT support_poc, owner_username, 
+          IF(device_id IS NULL, '', (SELECT CONCAT(serial, ' - ', hostname) FROM devices WHERE sds.device_id = devices.id)) AS device 
+          FROM sensitive_data_systems AS sds WHERE MONTH(review_date) = MONTH(CURRENT_DATE()) AND YEAR(review_date) = YEAR(CURRENT_DATE()) 
+          AND sds.deleted_at IS NULL"
+    records_array = ActiveRecord::Base.connection.exec_query(sql)
+    @result = []
+    @result.push({"table" => "sensitive_data_systems", "header" => records_array.columns, "rows" => records_array.rows, "total" => records_array.count})
 
     render turbo_stream: turbo_stream.replace(
       :reportListing,
@@ -24,55 +36,24 @@ class ReportsController < ApplicationController
 
   def systems_with_selected_data_type
 
-    sql = "SELECT used_by, third_party_product_service FROM dpa_exceptions WHERE deleted_at IS NULL AND data_type_id = " + params[:id]        
+    sql = "SELECT used_by, third_party_product_service FROM dpa_exceptions WHERE deleted_at IS NULL AND data_type_id = " + params[:data_type_id]
     records_array = ActiveRecord::Base.connection.exec_query(sql)
     @result = []
     @result.push({"table" => "dpa_exceptions", "header" => records_array.columns, "rows" => records_array.rows, "total" => records_array.count})
-    # header = records_array.columns.join(", ")  + "\n"
-    # csv.push(header)
-    # records_array.each do |row|
-    #   csv.push(row.values.join(", ")  + "\n")
-    # end
-    # csv.push("\n\n")
-    logger.debug "********************result one #{@result}"
-    # csv.push("it_security_incidents\n\n")
-    sql = "SELECT title, people_involved, equipment_involved FROM it_security_incidents WHERE deleted_at IS NULL AND data_type_id = " + params[:id]
+    
+    sql = "SELECT title, people_involved, equipment_involved FROM it_security_incidents WHERE deleted_at IS NULL AND data_type_id = " + params[:data_type_id]
     records_array = ActiveRecord::Base.connection.exec_query(sql)
     @result.push({"table" => "it_security_incidents", "header" => records_array.columns, "rows" => records_array.rows, "total" => records_array.count})
-    # records_array = ActiveRecord::Base.connection.exec_query(sql)
-    # header = records_array.columns.join(", ")  + "\n"
-    # csv.push(header)
-    # records_array.each do |row|
-    #   csv.push(row.values.join(", ")  + "\n")
-    # end
-    # csv.push("\n\n")
-    logger.debug "********************result two #{@result}"
-
-    # csv.push("legacy_os_records\n\n")
+    
     sql = "SELECT owner_username, legacy_os, hostname
-    FROM legacy_os_records as lor join devices as dev on lor.device_id = dev.id WHERE lor.deleted_at IS NULL AND data_type_id = " + params[:id]
+    FROM legacy_os_records as lor join devices as dev on lor.device_id = dev.id WHERE lor.deleted_at IS NULL AND data_type_id = " + params[:data_type_id]
     records_array = ActiveRecord::Base.connection.exec_query(sql)
     @result.push({"table" => "legacy_os_records", "header" => records_array.columns, "rows" => records_array.rows, "total" => records_array.count})
-    # header = records_array.columns.join(", ")  + "\n"
-    # csv.push(header)
-    # records_array.each do |row|
-    #   csv.push(row.values.join(", ")  + "\n")
-    # end
-    # csv.push("\n\n")
-    logger.debug "********************result three #{@result}"
-
-    # csv.push("sensitive_data_systems\n\n")
+    
     sql = "SELECT name, owner_username, (SELECT storage_locations.name FROM storage_locations WHERE sd.storage_location_id = storage_locations.id) AS storage_location, hostname
-    FROM sensitive_data_systems AS sd JOIN devices as dev on sd.device_id = dev.id WHERE sd.deleted_at IS NULL AND data_type_id = " + params[:id]
+    FROM sensitive_data_systems AS sd JOIN devices as dev on sd.device_id = dev.id WHERE sd.deleted_at IS NULL AND data_type_id = " + params[:data_type_id]
     records_array = ActiveRecord::Base.connection.exec_query(sql)
     @result.push({"table" => "sensitive_data_systems", "header" => records_array.columns, "rows" => records_array.rows, "total" => records_array.count})
-    # header = records_array.columns.join(", ")  + "\n"
-    # csv.push(header)
-    # records_array.each do |row|
-    #   csv.push(row.values.join(", ")  + "\n")
-    # end
-    logger.debug "********************result four #{@result}"
-
 
     render turbo_stream: turbo_stream.replace(
       :reportListing,
@@ -81,7 +62,7 @@ class ReportsController < ApplicationController
   end
 
   def permitted_params
-    params.permit!
+    params.permit(:data_type_id)
     # params.permit! # allow all parameters
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -59,6 +59,7 @@ Rails.application.routes.draw do
   resources :reports
   get 'legacy_os_records_review_date_next_month', to: 'reports#legacy_os_records_review_date_next_month', as: :legacy_os_records_review_date_next_month
   get 'systems_with_selected_data_type', to: 'reports#systems_with_selected_data_type', as: :systems_with_selected_data_type
+  get 'sensitive_data_system_review_date_this_month', to: 'reports#sensitive_data_system_review_date_this_month', as: :sensitive_data_system_review_date_this_month
 
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
