837c5515bd817be86405d99ee6f8af2609c93822
Margarita Barvinok <brita@umich.edu>
2021-08-07 00:39:43 -0400

    FIx fetch call

--- app/controllers/data_classification_levels_controller.rb
+++ app/controllers/data_classification_levels_controller.rb
@@ -1,7 +1,11 @@
 class DataClassificationLevelsController < ApplicationController
 
   def get_data_types
-    render json: DataType.where(data_classification_level_id: params[:id])
+    if params[:id] == "0"
+      render json: DataType.all
+    else
+      render json: DataType.where(data_classification_level_id: params[:id])
+    end
   end
   
 end
