48f30dd63c35aca4e9d806508b9b329bd06a4b20
Margarita Barvinok <brita@umich.edu>
2021-08-12 16:42:03 -0400

    remove/add stuff

--- app/javascript/controllers/report_controller.js
+++ app/javascript/controllers/report_controller.js
@@ -1,50 +1,45 @@
 import { Controller } from "stimulus"
-import Rails from "@rails/ujs"
 export default class ReportController extends Controller {
   static targets = ["form", "data_type", "classification_level", "review_month", "message",
-    "table", "end_date", "start_date", "end_date"]
+    "table", "end_date", "start_date", "end_date", "listing"]
 
   checkReviewMonth() {
     var table = this.tableTarget.value
-    var review_month = this.review_monthTarget.value
-    console.log("review_month")
-    console.log(review_month)
 
-    // failed attrempt to dissable revew_month dropdown list
-    // var review_month = this.review_monthTarget
-    // console.log("review_month.attributes")
-    // console.log(review_month.attributes)
-    // review_month.attributes.add('disabled')
-    // console.log(review_month.attributes[3])
-    // console.log(review_month.attributes[3].value)
-    // console.log(review_month.attributes['disabled'])
-
-    if (table != "isi" && review_month != "") {
-      console.log("here")
-      this.messageTarget.classList.add("device-error--hide")
-      this.messageTarget.classList.remove("device-error--display")
-      this.messageTarget.innerText = ""
+    if (table == "isi") {
+      this.review_monthTarget.disabled = true
     }
-    if (table == "isi" && review_month != "") {
-      this.messageTarget.classList.add("device-error--display")
-      this.messageTarget.classList.remove("device-error--hide")
-      this.messageTarget.innerText = "IT security table is not included into the Review month - select a different table or All";
+    else {
+      this.review_monthTarget.disabled = false
     }
   }
 
   checkTable() {
-    var table = this.tableTarget.value
     var review_month = this.review_monthTarget.value
-    if (table == "isi" && review_month != "") {
-      this.messageTarget.classList.add("device-error--display")
-      this.messageTarget.classList.remove("device-error--hide")
-      this.messageTarget.innerText = "IT security table is not included into the Review month - select a different table or All";
+    var selected = this.tableTarget.value
+    var dropdown = this.tableTarget;
+    dropdown.length = 0;
+    var defaultOption = document.createElement('option');
+    if (review_month) {
+      var data = { "dpa": "DPA exceptions", "lor": "Legacy OS records", "sds": "Sensitive data systems" }
     }
     else {
-      this.messageTarget.classList.add("device-error--hide")
-      this.messageTarget.classList.remove("device-error--display")
-      this.messageTarget.innerText = ""
+      var data = { "dpa": "DPA exceptions", "isi": "IT security incidents", "lor": "Legacy OS records", "sds": "Sensitive data systems" }
     }
+    defaultOption.value = 'all';
+    defaultOption.text = "All";
+    dropdown.add(defaultOption);
+    dropdown.selectedIndex = 0;
+    let option;
+
+    for (var [key, value] of Object.entries(data)) {
+      option = document.createElement('option');
+      option.value = key
+      option.text = value
+      option.selected = key === selected
+      dropdown.add(option);
+    }
+
   }
 
   changeClassification() {
@@ -62,7 +57,6 @@ export default class ReportController extends Controller {
         .then((data) => this.update_data_type(data)
         );
     }
-
   }
 
   update_data_type(data) {
@@ -86,14 +80,7 @@ export default class ReportController extends Controller {
   }
 
   submitForm(event) {
-    console.log("submt")
 
-    console.log(this.messageTarget.innerText)
-    // if (this.messageTarget.innerText == " ") {
-    //   console.log("here")
-    //   event.preventDefault()
-    // }
-    // else {
     var table = this.tableTarget.value
     var review_month = this.review_monthTarget.value
     var classification_level = this.classification_levelTarget.value
@@ -107,7 +94,6 @@ export default class ReportController extends Controller {
       this.messageTarget.classList.remove("device-error--hide")
       this.messageTarget.innerText = "Select a review month or data classification level or data type or date range";
       event.preventDefault()
-      return
     }
     else if (d_end < d_start) {
       this.messageTarget.classList.add("device-error--display")
@@ -115,70 +101,28 @@ export default class ReportController extends Controller {
       this.messageTarget.innerText = "[From] date should occur before [To] date";
       event.preventDefault()
     }
-    else if (review_month != "" && table == "isi") {
-      this.messageTarget.classList.add("device-error--display")
-      this.messageTarget.classList.remove("device-error--hide")
-      this.messageTarget.innerText = "IT security table is not included into the Review month - select a different table or All";
-      event.preventDefault()
-    }
+    // else if (review_month != "" && table == "isi") {
+    //   this.messageTarget.classList.add("device-error--display")
+    //   this.messageTarget.classList.remove("device-error--hide")
+    //   this.messageTarget.innerText = "IT security table is not included into the Review month - select a different table or All";
+    //   event.preventDefault()
+    // }
     else {
       console.log(this.messageTarget.classList)
       this.messageTarget.classList.add("device-error--hide")
       this.messageTarget.classList.remove("device-error--display")
       this.messageTarget.innerText = " "
     }
-    // }
-    // let start = this.start_dateTarget.value;
-    // let end = this.end_dateTarget.value;
-    // console.log(start)
-    // var d_start = Date.parse(start)
-    // console.log(d_start)
-    // var d_end = Date.parse(end)
-    // console.log(d_end)
-    // if (d_end < d_start) {
-    //   this.messageTarget.classList.add("device-error--display")
-    //   this.messageTarget.classList.remove("device-error--hide")
-    //   this.messageTarget.innerText = "[From] date should occur before [To] date";
-    //   event.preventDefault()
-    // }
-    // else {
-    //   console.log(this.messageTarget.classList)
-    //   this.messageTarget.classList.add("device-error--hide")
-    //   this.messageTarget.classList.remove("device-error--display")
-    //   this.messageTarget.innerText = ""
-    // }
-
   }
 
   clearFilters() {
-    this.element.reset()
+    this.formTarget.reset()
     this.changeClassification()
-
-    console.log(this.messageTarget.classList)
+    this.review_monthTarget.disabled = false
     this.messageTarget.classList.add("device-error--hide")
     this.messageTarget.classList.remove("device-error--display")
     this.messageTarget.innerText = " "
-    console.log("in clearFilters")
-    // this.clearReport()
+    this.listingTarget.innerText = ""
   }
-
-  clearReport() {
-    console.log("hell")
-    // var data = new FormData();
-    fetch(`/run_report?table=all&review_month=current&data_classification_level_id=&data_type_id=&report_data%5Bstart_date%5D=&report_data%5Bend_date%5D=&format=html&commit=Clear`)
-
-    // Rails.ajax({
-    //   url: '/run_report',
-    //   type: 'GET',
-    //   data: { commit: 'Clear' }
-    // })
-
-  }
-
-  // get reportController() {
-  //   console.log("hell")
-  //   console.log(this.application.getControllerForElementAndIdentifier(element, "run_report"))
-  //   return this.application.getControllerForElementAndIdentifier(element, "run_report")
-  // }
 
 }
