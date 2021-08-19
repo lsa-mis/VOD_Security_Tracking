5185ef27a4b0d1706b04b1bd65015970fecf2006
Rick Smoke <rsmoke@umich.edu>
2021-08-19 18:33:24 -0400

    add class to export declaration

--- app/javascript/controllers/filters_controller.js
+++ app/javascript/controllers/filters_controller.js
@@ -1,6 +1,6 @@
 import { Controller } from "stimulus"
 // const Choices = require('choices.js');
-export default class extends Controller {
+export default class FiltersController extends Controller {
 
   clearFilters() {
     this.element.reset()
