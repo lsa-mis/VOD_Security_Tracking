c7fd302a8521b146e098b9319fbe9d28d273c9fd
Margarita Barvinok <brita@umich.edu>
2021-08-19 10:34:31 -0400

    Add logic to search by serial or hostname

--- app/javascript/controllers/filters_controller.js
+++ app/javascript/controllers/filters_controller.js
@@ -1,8 +1,31 @@
 import { Controller } from "stimulus"
 // const Choices = require('choices.js');
 export default class extends Controller {
+  static targets = ["device_id", "serial", "hostname", "device_error"]
+
+  updateDeviceFilter() {
+    var hostname = this.hostnameTarget.value
+    var serial = this.serialTarget.value
+
+    var device_id = this.device_idTarget
+    if (serial != "" && hostname != "") {
+      this.device_errorTarget.classList.add("device-error--display")
+      this.device_errorTarget.classList.remove("device-error--hide")
+    }
+    else if (serial != '') {
+      this.device_idTarget.value = serial
+      this.device_errorTarget.classList.remove("device-error--display")
+      this.device_errorTarget.classList.add("device-error--hide")
+    }
+    else if (hostname != '') {
+      this.device_idTarget.value = hostname
+      this.device_errorTarget.classList.remove("device-error--display")
+      this.device_errorTarget.classList.add("device-error--hide")
+    }
+  }
 
   clearFilters() {
     this.element.reset()
+    this.device_idTarget.value = ""
   }
 }
