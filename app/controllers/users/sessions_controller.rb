c5e86a01560a96a2195e60c3cd1a547ec37c2244
Margarita Barvinok <brita@umich.edu>
2021-09-03 09:39:01 -0400

    Change create membership array

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -32,12 +32,25 @@ class Users::SessionsController < Devise::SessionsController
 
     def after_login
       if user_signed_in?
+        ldap_config = { :host => "adsroot.itcs.umich.edu", :port => 389, :encryption => nil, :base_dn => "OU=UMICH,DC=adsroot,DC=itcs,DC=umich,DC=edu",
+                :group_base => "OU=UMICH,DC=adsroot,DC=itcs,DC=umich,DC=edu", :attr_login => "sAMAccountName", :server_type => :active_directory,
+                :service_user => Rails.application.credentials.ldap_admin[:user], :search_filter => "(objectClass=*)", :service_pass => Rails.application.credentials.ldap_admin[:password],
+                :anon_queries => false }
+
+        fluff = LdapFluff.new(ldap_config)
         membership = []
-        groups = Devise::LDAP::Adapter.get_ldap_param(current_user.username,'memberOf')
-        groups.each do |group|
-          g = group.split(',')
-          membership.append(g.first.remove("CN="))
+        # groups = Devise::LDAP::Adapter.get_ldap_param(current_user.username,'memberOf')
+        # groups.each do |group|
+        #   g = group.split(',')
+        #   membership.append(g.first.remove("CN="))
+        # end
+        access_groups = AccessLookup.pluck(:ldap_group).uniq
+        access_groups.each do |group|
+          if fluff.is_in_groups?(current_user.username,Array.wrap(group))
+            membership.append(group)
+          end
         end
+        Rails.logger.debug "********************* membership #{membership}"
         session[:user_memberships] = membership
       end
     end
