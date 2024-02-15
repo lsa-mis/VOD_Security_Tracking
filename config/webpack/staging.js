2179b4cd7706eaefef13099e7b4c9340f0574cf0
Margarita Barvinok <brita@umich.edu>
2024-02-15 08:35:53 -0500

    add memory to webpack

--- config/webpack/staging.js
+++ config/webpack/staging.js
@@ -3,3 +3,14 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'staging'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
+
+const merge = require('webpack-merge');
+
+module.exports = merge(common, {
+    mode: 'staging',
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
+    }
+});
