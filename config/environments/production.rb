f4b3e4a855909c2fe2d60dd4a13467a8d506bb04
rsmokeUM <rsmoke@umich.edu>
2025-01-07 15:37:33 -0500

    Update production environment configuration for asset management
    - Set CSS compressor to nil in production.rb to disable CSS preprocessing.
    - Improved comments for clarity regarding Action Mailer caching behavior.
    - These changes enhance the asset management process and provide clearer documentation for future reference.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress CSS using a preprocessor.
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = nil
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
@@ -66,14 +66,14 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "app_production"
 
-  # The `perform_caching` option determines whether or not Action Mailer will 
-  # cache emails. When set to `true`, Action Mailer will cache emails to improve 
-  # performance. This can be useful if your application sends out a lot of emails 
-  # that are identical or very similar, as it can reduce the amount of work the 
+  # The `perform_caching` option determines whether or not Action Mailer will
+  # cache emails. When set to `true`, Action Mailer will cache emails to improve
+  # performance. This can be useful if your application sends out a lot of emails
+  # that are identical or very similar, as it can reduce the amount of work the
   # server has to do.
-  # When set to `false`, which means that Action Mailer will not cache emails. 
-  # This might be because the emails your application sends are unique, so caching 
-  # wouldn't provide a performance benefit. Or it might be to avoid potential issues 
+  # When set to `false`, which means that Action Mailer will not cache emails.
+  # This might be because the emails your application sends are unique, so caching
+  # wouldn't provide a performance benefit. Or it might be to avoid potential issues
   # with stale or incorrect emails being sent out due to caching.
   config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
