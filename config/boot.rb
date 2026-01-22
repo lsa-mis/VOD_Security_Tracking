e41116376fd4b9089816e1cd62d0c95c4671c54d
rsmokeUM <rsmoke@umich.edu>
2026-01-22 16:38:42 -0500

    Ensure Logger is defined before ActiveSupport loads in boot configuration

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,7 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
+# Rails 6.1 + Ruby 3.3: ensure Logger is defined before ActiveSupport loads.
+require "logger"
+
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
